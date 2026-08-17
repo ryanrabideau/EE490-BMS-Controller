3e5ff8ced1e5efc407ccd9c327d3d816b04abe38
BOSENDORFER-5I\Michael <michaelcollins.ece@gmail.com>
2026-08-17 18:56:10 -0400

    Balancing between 2 cells using D8 and D2

--- firmware/EE490BMS/Core/Inc/main.h
+++ firmware/EE490BMS/Core/Inc/main.h
@@ -67,6 +67,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define SW1_Pin GPIO_PIN_9
+#define SW1_GPIO_Port GPIOA
+#define SW2_Pin GPIO_PIN_10
+#define SW2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
--- firmware/EE490BMS/Core/Src/main.c
+++ firmware/EE490BMS/Core/Src/main.c
@@ -107,8 +107,22 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  TIM1->CCR1 = 800;
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	HAL_GPIO_WritePin(GPIOA, LD2_Pin, GPIO_PIN_SET);
+	HAL_Delay(1000);
+	HAL_GPIO_WritePin(GPIOA, LD2_Pin, GPIO_PIN_RESET);
+	while (1)
+	{
+
+		HAL_GPIO_WritePin(GPIOA, SW1_Pin, GPIO_PIN_SET);
+		HAL_Delay(15);
+		HAL_GPIO_WritePin(GPIOA, SW1_Pin, GPIO_PIN_RESET);
+		HAL_Delay(3);
+
+		HAL_GPIO_WritePin(GPIOA, SW2_Pin, GPIO_PIN_SET);
+		HAL_Delay(15);
+		HAL_GPIO_WritePin(GPIOA, SW2_Pin, GPIO_PIN_RESET);
+		HAL_Delay(3);
+	}
 
   /* USER CODE END 2 */
 
@@ -348,7 +362,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|SW1_Pin|SW2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -356,12 +370,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LD2_Pin SW1_Pin SW2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|SW1_Pin|SW2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
